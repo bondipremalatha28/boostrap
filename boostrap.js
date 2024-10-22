5d105f0972054af9de01630f591180654601917f
bondipremalatha28 <bondipremalatha@gmail.comm>
2024-10-22 10:15:10 +0530

    first commit

---
+++ boostrap.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.logo').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 500,
+      });
+    
+  });
